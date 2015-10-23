d0785806268872dc528ac6b24ee0a684482510a2
Ryan Jones <ryan@system88.com>
2015-10-23 12:12:49 -0600

    add in tests, refactor to be a bit more OOP, can still be broken down but good enough for now

--- lib/lita/handlers/animate.rb
+++ lib/lita/handlers/animate.rb
@@ -1,6 +1,8 @@
 module Lita
   module Handlers
     class Animate < Handler
+      attr_accessor :search_query
+
       URL = "https://ajax.googleapis.com/ajax/services/search/images"
 
       route(/(?:animate|gif|anim)(?:\s+me)? (.+)/i, :fetch, command: true, help: {
@@ -12,24 +14,17 @@ module Lita
       end
 
       def fetch(response)
-        query = "#{response.matches[0][0]} gif"
+        self.search_query = "#{response.matches[0][0]} gif"
 
-        http_response = http.get(
-          URL,
-          v: "1.0",
-          q: query,
-          safe: safe_value,
-          rsz: 8,
-          imgtype: "animated"
-        )
+        search_response = query_google
 
-        data = MultiJson.load(http_response.body)
+        search_payload = MultiJson.load(search_response.body)
 
-        if data["responseStatus"] == 200
+        if search_payload["responseStatus"] == 200
           choice = nil
 
-          data["responseData"]["results"].shuffle.each do |result|
-            if /.*\.gif/.match(result["url"])
+          search_payload["responseData"]["results"].shuffle.each do |result|
+            if valid_extension?(result["unescapedUrl"])
               choice = result
               break
             end
@@ -38,13 +33,14 @@ module Lita
           if choice
             response.reply "#{choice["unescapedUrl"]}"
           else
-            response.reply "No gifs found for '#{query}' (shrug)."
+            response.reply "No gifs found for '#{search_query}' (shrug)."
           end
         else
-          reason = data["responseDetails"] || "unknown error"
+          reason = search_payload["responseDetails"] || "unknown error"
           Lita.logger.warn(
             "Couldn't get image from Google: #{reason}"
           )
+          response.reply "(facepalm) An error has occurred when querying Google. Please check Lita's logs."
         end
       end
 
@@ -55,6 +51,27 @@ module Lita
         safe = safe.to_s.downcase
         safe = "active" unless ["active", "moderate", "off"].include?(safe)
         safe
+      end
+
+      def valid_extension?(result)
+        if /^.*\.(jpg|JPG|png|PNG|jpeg|JPEG)/.match(result)
+          false
+        elsif /.*\.gif$/.match(result)
+          true
+        else
+          false
+        end
+      end
+
+      def query_google
+        Faraday.get(
+          URL,
+          v: "1.0",
+          q: search_query,
+          safe: safe_value,
+          rsz: 8,
+          imgtype: "animated"
+        )
       end
     end
 
