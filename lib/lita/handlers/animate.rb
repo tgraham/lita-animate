a36e290618628e40cef55d635aac63f60381002f
Ryan Jones <ryan@system88.com>
2015-11-02 13:24:57 -0700

    more OOP/SR

--- lib/lita/handlers/animate.rb
+++ lib/lita/handlers/animate.rb
@@ -1,11 +1,11 @@
 module Lita
   module Handlers
     class Animate < Handler
-      attr_accessor :search_query
+      attr_accessor :response, :search_query, :search_response, :search_results, :gif
 
       URL = "https://ajax.googleapis.com/ajax/services/search/images"
 
-      route(/(?:animate|gif|anim)(?:\s+me)? (.+)/i, :fetch, command: true, help: {
+      route(/(?:animate|gif|anim)(?:\s+me)? (.+)/i, :init, command: true, help: {
         "animate QUERY" => "animate everything"
       })
 
@@ -13,54 +13,63 @@ module Lita
         handler_config.safe_search = :active
       end
 
-      def fetch(response)
+      def init(response)
+        self.response = response
+        fetch
+      end
+
+      def fetch
         self.search_query = "#{response.matches[0][0]} gif"
+        self.search_response = query_google
+        self.search_results = MultiJson.load(search_response.body)
 
-        search_response = query_google
-
-        search_payload = MultiJson.load(search_response.body)
-
-        if search_payload["responseStatus"] == 200
-          choice = nil
-
-          search_payload["responseData"]["results"].shuffle.each do |result|
-            if  /^((?!jpg|jpeg|png).)*.gif$/i.match(result["unescapedUrl"])
-              choice = result
-              break
-            end
-          end
-
-          if choice
-            response.reply "#{choice["unescapedUrl"]}"
-          else
-            response.reply "No gifs found for '#{search_query}' (shrug)."
-          end
+        if valid_response?
+          reply
         else
-          reason = search_payload["responseDetails"] || "unknown error"
-          Lita.logger.warn(
-            "Couldn't get image from Google: #{reason}"
-          )
-          response.reply "(facepalm) An error has occurred when querying Google. Please check Lita's logs."
+          invalid_reply
         end
       end
 
       private
+
+      def invalid_reply
+        reason = search_response["responseDetails"] || "unknown error"
+        Lita.logger.warn(
+          "Couldn't get image from Google: #{reason}"
+        )
+        response.reply "(facepalm) An error has occurred when querying Google. Please check Lita's logs."
+      end
+
+      def reply
+        if gif
+          response.reply "#{gif["unescapedUrl"]}"
+        else
+          response.reply "No gifs found for '#{search_query}' (shrug)."
+        end
+      end
+
+      def gif
+        @gif ||= set_gif
+      end
+
+      def set_gif
+        search_results["responseData"]["results"].shuffle.each do |result|
+          if  /^((?!jpg|jpeg|png).)*.gif$/i.match(result["unescapedUrl"])
+            return result
+          end
+        end
+        nil
+      end
+
+      def valid_response?
+        search_results["responseStatus"] == 200
+      end
 
       def safe_value
         safe = Lita.config.handlers.animate.safe_search || "active"
         safe = safe.to_s.downcase
         safe = "active" unless ["active", "moderate", "off"].include?(safe)
         safe
-      end
-
-      def valid_extension?(result)
-        if /^.*\.(jpg|png|jpeg)/i.match(result)
-          false
-        elsif /.*\.gif$/.match(result)
-          true
-        else
-          false
-        end
       end
 
       def query_google
